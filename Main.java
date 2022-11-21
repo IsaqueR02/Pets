4a1b1f70078683c97f67e79e7665935c9bae78ba
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2022-11-21 20:58:29 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Cachorro cao = new Cachorro ("Max", "Rottweiler", 55);
+    Fish peixe = new Fish("Rover", "Agua doce", 1);
+    
+System.out.println("Cachorro" + cao);
+System.out.println("Peixe" + peixe);
+  }
+}
